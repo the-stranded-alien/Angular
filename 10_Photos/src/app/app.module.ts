dc9ebb8b566bd31ce67da5ddb1c2775a9716e192
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-08-22 23:58:47 +0530

    Another example

---
+++ 10_Photos/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PhotoShowComponent } from './photo-show/photo-show.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PhotoShowComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
