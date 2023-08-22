99e4f61cd6f11bad947015039de8dbca1ff856bb
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-08-22 23:15:56 +0530

    Using RxJs with TS in Angular

--- 08_WikiSearch/src/app/wikipedia.service.ts
+++ 08_WikiSearch/src/app/wikipedia.service.ts
@@ -1,5 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, pluck } from 'rxjs';
+
+// const observable = new Observable<number>(observer => {
+//   observer.next(1);
+// });
+
+// observable.subscribe(value => {
+//   console.log(value);
+// });
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      title: string,
+      snippet: string,
+      pageid: number
+    }[];
+  };
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +30,17 @@ export class WikipediaService {
 
   search(term: string) {
     const url = `https://en.wikipedia.org/w/api.php`;
-    return this.http.get(url, {
-      params: {
-        action: 'query',
-        format: 'json',
-        list: 'search',
-        utf8: '1',
-        srsearch: term,
-        origin: '*'
-      }
-    });
+    return this.http
+      .get<WikipediaResponse>(url, {
+        params: {
+          action: 'query',
+          format: 'json',
+          list: 'search',
+          utf8: '1',
+          srsearch: term,
+          origin: '*'
+        }
+      })
+      .pipe(pluck('query', 'search'));
   }
 }
